dfa4318db8f6c1c09280d797f95ac4e6c2468861
vi_vio <77623828+vivio2115@users.noreply.github.com>
2025-02-08 13:03:07 +0100

    Added levels and some improvments
    I added the following levels:
    - Easy
    - Meadium
    - Hard
    
    Easy simply works in such a way that it selects a random field meadium tries to block the player and hard does it all at once, that is, it tries to block the player looking for a field that will give him a win and if there is no such field then random.
    
    I also added a very simple loop to loop the whole game so you don't have to run it all the time.

--- main.c
+++ main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 #include <time.h>
 
 char board[3][3];
 const char PLAYER = 'X';
 const char COMPUTER = 'O';
+int difficulty;
 
 void restBoard();
 void printBoard();
@@ -15,36 +15,218 @@ void computerMove();
 char checkWinner();
 void printWinner(char);
 
+// TODO: Upgrade the look of the menu and game
 int main()
 {
-    char winner = ' ';
+    printf("Welcome to Tic-Tac-Toe!\n");
+    printf("Choose difficulty (1-3): \n");
+    printf("1 - Easy\n");
+    printf("2 - Medium\n");
+    printf("3 - Hard\n");
+    do {
+        printf("Enter difficulty: ");
+        scanf("%d", &difficulty);
 
-    restBoard();
+        if (difficulty < 1 || difficulty > 3) {
+            printf("Invalid input. Please enter a number between 1 and 3.\n");
+        }
+    } while (difficulty < 1 || difficulty > 3);
 
-    while (winner == ' ' && checkFreeSpaces() > 0)
-    {
-        printBoard();
+    while (1) {
+        char winner = ' ';
 
-        playerMove();
-        winner = checkWinner();
-        if (winner != ' ' || checkFreeSpaces() == 0) {
-            break;
+        restBoard();
+
+        while (winner == ' ' && checkFreeSpaces() > 0)
+        {
+            printBoard();
+
+            playerMove();
+            winner = checkWinner();
+            if (winner != ' ' || checkFreeSpaces() == 0) {
+                break;
+            }
+
+            computerMove();
+            winner = checkWinner();
+            if (winner != ' ' || checkFreeSpaces() == 0) {
+                break;
+            }
         }
 
-        computerMove();
-        winner = checkWinner();
-        if (winner != ' ' || checkFreeSpaces() == 0) {
-            break;
+        printBoard();
+        printWinner(winner);
+
+        char choice;
+        while (1) {
+            printf("Do you want to play again? (y/n): ");
+            scanf(" %c", &choice);
+            if (choice == 'y' || choice == 'Y') {
+                break;
+            }
+            else if (choice == 'n' || choice == 'N') {
+                printf("Thanks for playing!\n");
+                printf("Press any key to exit...\n");
+                getchar();
+                getchar();
+                return 0;
+            }
         }
     }
 
-    printBoard();
-    printWinner(winner);
 
-    printf("Press Enter to exit...");
-    getchar();
-    getchar();
+    return 0;
+}
+int blockPlayerMove()
+{
+    for (int i = 0; i < 3; i++) {
+        if (board[i][0] == PLAYER && board[i][1] == PLAYER && board[i][2] == ' ') {
+            board[i][2] = COMPUTER;
+            return 1;
+        }
+        if (board[i][1] == PLAYER && board[i][2] == PLAYER && board[i][0] == ' ') {
+            board[i][0] = COMPUTER;
+            return 1;
+        }
+        if (board[i][0] == PLAYER && board[i][2] == PLAYER && board[i][1] == ' ') {
+            board[i][1] = COMPUTER;
+            return 1;
+        }
+    }
 
+    for (int i = 0; i < 3; i++) {
+        if (board[0][i] == PLAYER && board[1][i] == PLAYER && board[2][i] == ' ') {
+            board[2][i] = COMPUTER;
+            return 1;
+        }
+        if (board[1][i] == PLAYER && board[2][i] == PLAYER && board[0][i] == ' ') {
+            board[0][i] = COMPUTER;
+            return 1;
+        }
+        if (board[0][i] == PLAYER && board[2][i] == PLAYER && board[1][i] == ' ') {
+            board[1][i] = COMPUTER;
+            return 1;
+        }
+    }
+
+    if (board[0][0] == PLAYER && board[1][1] == PLAYER && board[2][2] == ' ') {
+        board[2][2] = COMPUTER;
+        return 1;
+    }
+    if (board[1][1] == PLAYER && board[2][2] == PLAYER && board[0][0] == ' ') {
+        board[0][0] = COMPUTER;
+        return 1;
+    }
+    if (board[0][0] == PLAYER && board[2][2] == PLAYER && board[1][1] == ' ') {
+        board[1][1] = COMPUTER;
+        return 1;
+    }
+
+    if (board[0][2] == PLAYER && board[1][1] == PLAYER && board[2][0] == ' ') {
+        board[2][0] = COMPUTER;
+        return 1;
+    }
+    if (board[1][1] == PLAYER && board[2][0] == PLAYER && board[0][2] == ' ') {
+        board[0][2] = COMPUTER;
+        return 1;
+    }
+    if (board[0][2] == PLAYER && board[2][0] == PLAYER && board[1][1] == ' ') {
+        board[1][1] = COMPUTER;
+        return 1;
+    }
+
+    return 0;
+}
+
+int tryToWin() {
+    for (int i = 0; i < 3; i++) {
+        //check rows
+        if (board[i][0] == COMPUTER && board[i][1] == COMPUTER && board[i][2] == ' ') {
+            board[i][2] = COMPUTER;
+            return 1;
+        }
+        if (board[i][1] == COMPUTER && board[i][2] == COMPUTER && board[i][0] == ' ') {
+            board[i][0] = COMPUTER;
+            return 1;
+        }
+        if (board[i][0] == COMPUTER && board[i][2] == COMPUTER && board[i][1] == ' ') {
+            board[i][1] = COMPUTER;
+            return 1;
+        }
+
+        //check columns (xD)
+        if (board[0][i] == COMPUTER && board[1][i] == COMPUTER && board[2][i] == ' ') {
+            board[2][i] = COMPUTER;
+            return 1;
+        }
+        if (board[1][i] == COMPUTER && board[2][i] == COMPUTER && board[0][i] == ' ') {
+            board[0][i] = COMPUTER;
+            return 1;
+        }
+        if (board[0][i] == COMPUTER && board[2][i] == COMPUTER && board[1][i] == ' ') {
+            board[1][i] = COMPUTER;
+            return 1;
+        }
+    }
+
+    //check diagonals
+    if (board[0][0] == COMPUTER && board[1][1] == COMPUTER && board[2][2] == ' ') {
+        board[2][2] = COMPUTER;
+        return 1;
+    }
+    if (board[1][1] == COMPUTER && board[2][2] == COMPUTER && board[0][0] == ' ') {
+        board[0][0] = COMPUTER;
+        return 1;
+    }
+    if (board[0][0] == COMPUTER && board[2][2] == COMPUTER && board[1][1] == ' ') {
+        board[1][1] = COMPUTER;
+        return 1;
+    }
+
+    if (board[0][2] == COMPUTER && board[1][1] == COMPUTER && board[2][0] == ' ') {
+        board[2][0] = COMPUTER;
+        return 1;
+    }
+    if (board[1][1] == COMPUTER && board[2][0] == COMPUTER && board[0][2] == ' ') {
+        board[0][2] = COMPUTER;
+        return 1;
+    }
+    if (board[0][2] == COMPUTER && board[2][0] == COMPUTER && board[1][1] == ' ') {
+        board[1][1] = COMPUTER;
+        return 1;
+    }
+
+    return 0;
+}
+
+int pickBestSpot() {
+    //in the middel
+    if (board[1][1] == ' ') {
+        board[1][1] = COMPUTER;
+        return 1;
+    }
+    //in the corner
+    if (board[0][0] == ' ') {
+        board[0][0] = COMPUTER;
+    }
+    if (board[2][0] == ' ') {
+        board[2][0] = COMPUTER;
+    }
+    if (board[0][2] == ' ') {
+        board[0][0] = COMPUTER;
+    }
+    if (board[2][2] == ' ') {
+        board[2][2] = COMPUTER;
+    }
+    //just random lamo
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (board[i][j] == ' ') {
+                board[i][j] = COMPUTER;
+                return 1;
+            }
+        }
+    }
 
     return 0;
 }
@@ -106,13 +288,44 @@ void playerMove()
         }
     }
 }
-// TODO: make better computers moves and add difficulty levels
+
 void computerMove()
 {
     srand(time(0));
     int x, y;
 
-    if (checkFreeSpaces() > 0) {
+    if (checkFreeSpaces() == 0) {
+        return;
+    }
+    if (difficulty == 1) {
+        //easy (lamo)
+        do {
+            x = rand() % 3;
+            y = rand() % 3;
+        } while (board[x][y] != ' ');
+
+        board[x][y] = COMPUTER;
+    } else if (difficulty == 2) {
+        //medium
+        if (!blockPlayerMove()) {
+            do {
+                x = rand() % 3;
+                y = rand() % 3;
+            } while (board[x][y] != ' ');
+
+            board[x][y] = COMPUTER;
+        }
+    } else if (difficulty == 3) {
+        //hard
+        if (tryToWin()) {
+            return;
+        }
+        if (blockPlayerMove()) {
+            return;
+        }
+        if (pickBestSpot()) {
+            return;
+        }
         do {
             x = rand() % 3;
             y = rand() % 3;

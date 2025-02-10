28b84434a2f03eabf560d12edd6ab83888e97295
vi_vio <77623828+vivio2115@users.noreply.github.com>
2025-02-10 22:06:45 +0100

    Create acsii.h

---
+++ acsii.h
@@ -0,0 +1,50 @@
+/*
+ * ACSII color list made by vivio2115 (Github) last updated on 10/02/2025.
+ * Its free to use and its under MIT license.
+ */
+
+#ifndef ACSII_H
+#define ACSII_H
+ //regular
+#define BLK "\x1B[0;30m" //black
+#define RED "\x1B[0;31m"  //red
+#define GRN "\x1B[0;32m" //green
+#define YEL "\x1B[0;33m" //yellow
+#define BLU "\x1B[0;34m" //blue
+#define MAG "\x1B[0;35m" //purple
+#define CYN "\x1B[0;36m" //cyan
+#define WHT "\x1B[0;37m" //white
+//bold
+#define BBLK "\x1B[1;30m" //black bold
+#define BRED "\x1B[1;31m" //red bold
+#define BGRN "\x1B[1;32m" //green bold
+#define BYEL "\x1B[1;33m" //yellow bold
+#define BBLU "\x1B[1;34m" //blue bold
+#define BMAG "\x1B[1;35m" //purple bold
+#define BCYN "\x1B[1;36m" //cyan bold
+#define BWHT "\x1B[1;37m" //white bold
+
+// underline
+#define UBLK "\x1B[4;30m" //black underline
+#define URED "\x1B[4;31m" //red underline
+#define UGRN "\x1B[4;32m" //green underline
+#define UYEL "\x1B[4;33m" //yellow underline
+#define UBLU "\x1B[4;34m" //blue underline
+#define UMAG "\x1B[4;35m" //purple underline
+#define UCYN "\x1B[4;36m" //cyan underline
+#define UWHT "\x1B[4;37m" //white underline
+
+// backbround
+#define BLKB "\x1B[40m"  //black
+#define REDB "\x1B[41m"  //red
+#define GRNB "\x1B[42m" //green
+#define YELB "\x1B[43m" //yellow
+#define BLUB "\x1B[44m" //blue
+#define MAGB "\x1B[45m" //purple
+#define CYNB "\x1B[46m" //cyan
+#define WHTB "\x1B[47m" //white
+
+//rest
+#define reset "\x1B[0m" //reset
+
+#endif //ACSII_H
